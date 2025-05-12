fe5532712a3245d44414b594e662e6fc1081b678
Reyneil Rodelas <156262539+crazymix26@users.noreply.github.com>
2025-05-12 13:44:45 +0800

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <section className="w-full h-screen flex flex-col justify-center items-center text-center bg-gradient-to-b from-blue-900 to-black">
         <div className="max-w-4xl mx-auto">
           <h1 className="text-5xl font-extrabold text-white mb-4">
-            Turning bold ideas into real-world impact <span className="text-blue-400">---One Startup at a Time.</span>
+            Turning bold ideas into real-world impact <span className="text-blue-400">— One Startup at a Time.</span>
           </h1>
           <p className="text-lg text-gray-300 mb-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet do
